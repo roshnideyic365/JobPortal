6a8ebacb34778921daa642667ebeeb67826a003e
ROSHNI DEY <roshnideyic365@gmail.com>
2021-11-09 20:37:39 +0530

    Add files via upload

---
+++ JobPortal new/about.php
@@ -0,0 +1,78 @@
+<?php include 'header.php'?>
+
+<div class="content">
+    <img src="banner-1.jpg" style="height: 50vh; width:100%;">
+    <div class="container">
+        <div class="accordion" id="accordionExample">
+            <div class="card">
+                <div class="card-header" id="headingZero">
+                    <h2>Let's make your next great hire!</h2>
+                </div>
+            </div>
+            <div class="card">
+              <div class="card-header" id="headingOne">
+                <h2 class="mb-0">
+                  <button class="btn btn-light btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    1. Create your free account <i class="fas fa-chevron-down fa-0.5x" style="position: relative; float: right;"></i>
+                  </button>
+                </h2>
+              </div>
+          
+              <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                <div class="card-body">
+                  <div class="details">
+                    All you need is your email address to create an account and start building your job post
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            <div class="card">
+              <div class="card-header" id="headingTwo">
+                <h2 class="mb-0">
+                  <button class="btn btn-light btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseOne">
+                    2. Build your job post <i class="fas fa-chevron-down fa-0.5x" style="position: relative; float: right;"></i>
+                  </button>
+                </h2>
+              </div>
+          
+              <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                <div class="card-body">
+                  <div class="details">
+                  Then just add a title, description, and location to your job post, and you're ready to go.
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="card-header" id="headingThree">
+                <h2 class="mb-0">
+                  <button class="btn btn-light btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseOne">
+                    3. Post your job <i class="fas fa-chevron-down fa-0.5x" style="position: relative; float: right;"></i>
+                  </button>
+                </h2>
+              </div>
+          
+              <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                <div class="card-body">
+                  <div class="details">
+                  After you post your job use our state of the art tools to help you find dream talent.
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="card">
+              <div class="card-header" id="headingZero">
+                <p><i class="far fa-lightbulb" style="padding-right: 1%;"></i> For more details <a href="contact.php"> Contact Us</a>.</p>
+              </div>
+          </div>
+          </div>
+    </div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+</div>
---
+++ JobPortal new/contact.php
@@ -0,0 +1,17 @@
+<?php include 'header.php'?>
+
+<div class="content">
+    <div class="contact_container">
+        <h2>CONTACT US</h2>
+        <div class="g_map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3340544.4960977174!2d-121.88102652009785!3d35.150373310697375!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fba02425dad8f%3A0x6c296c66619367e0!2sGoogleplex!5e0!3m2!1sen!2sin!4v1628463464648!5m2!1sen!2sin" width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+        <div class="contact_content">
+            <p>We want to hear from you! Drop us a note and someone from our team will get back to you.
+                Looking for a job? Call +1111100000
+                Have a question that involves our Job Portal? Please head on over to our Locations page to reach our locations.</p>
+            <p><b>Call: +111100000</b></p>
+            <p><b>Mail: support.jobportal@gmail.com</b></p>
+        </div>
+    </div>
+</div>
