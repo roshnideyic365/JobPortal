ba6a9dcc5c07c6a94919aa24284e6eaf850a3f9e
ROSHNI DEY <roshnideyic365@gmail.com>
2021-11-09 20:39:00 +0530

    Add files via upload

---
+++ register.php
@@ -0,0 +1,83 @@
+<?php 
+$server='localhost';
+$username='root';
+$password='';
+$database='jobs';
+
+$conn= mysqli_connect($server,$username,$password,$database);
+
+if($conn->connect_error){
+	die("Connection failed:".$conn->connect_error);
+
+}
+echo"";
+
+if(isset($_POST['submit'])){
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $number=$_POST['phone_no'];
+    $password=$_POST['password'];
+
+    $sql = "INSERT INTO `user`(`Name`, `email`, `password`,`phone_no`) VALUES ('$name','$email','$password','$number')";
+    if(mysqli_query($conn, $sql)){
+        echo "Records inserted successfully.";
+    } else{
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+	<style>
+		body{
+			background-image: url('backgroundimage.jpg');
+			background-size: cover;
+		}
+		form{
+			background-color: #fff;
+			margin-top: 6em;
+			margin-left: 30em;
+			margin-right: 10em;
+			padding: 30px;
+			box-shadow: 10px 10px 8px 10px #888888;
+		}
+	</style>
+	<title>Register</title>
+</head>
+<body>
+	<div class="container">
+	<form method="POST">
+	<div class="mb-3">
+    <label for="exampleInputName" class="form-label">Full Name</label>
+    <input type="text" class="form-control" id="exampleInputName" name="name" required>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" required>
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputNumber" class="form-label">Phone Number</label>
+    <input type="number" class="form-control" id="exampleInputNumber"name="phone_no"  required>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" name="password" required>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword2" class="form-label">Confirm Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword2" required>
+  </div>
+  <input type="submit" class="btn btn-primary" name="submit">
+  <br>
+<br>
+  Already Registered? <a href="Login.php">Login</a>
+</form>
+	</div>
+</body>
+</html>
